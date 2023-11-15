d6719d60ff50f23d8a204c5b5e3c248acb6f1ff0
rjara <rjara@CA-0246>
2023-11-15 23:03:07 +0100

    hencing the names of some methods

--- src/locals/adapter.tsx
+++ src/locals/adapter.tsx
@@ -1,181 +1,181 @@
-import { group } from "console";
-import { mutationPostUser, mutationPutUser, queryGetUser } from "../hooks/group.hook";
+import { mutationPutUser, queryGetUser } from "../hooks/group.hook";
 import { IGroup } from "../interfaces/IGroup";
 import { IUser } from "../interfaces/IUser";
 import { User } from "../models/User";
 import { checkGroupConsistency, globalUserDefault } from "../util/util";
 // import { localGroups, setLocalGroups } from "./group.local";
 import { Group } from "../models/Group";
-import { UserInfo } from "os";
 import { IUserInfo } from "../interfaces/IUserInfo.js";
 import { Bugfender } from "@bugfender/sdk";
 
-
 const getUserFromAPI = async (idUser: string) => {
-    const resp = (await queryGetUser(idUser)).data;
+  const resp = (await queryGetUser(idUser)).data;
 
-    return User.newUser(resp);
-}
+  return User.newUser(resp);
+};
 
 const getGroup = async (idUser: string, idGroup: string) => {
-    if (idGroup)
-    {
-        Bugfender.log('idUser:' + idUser);
-        let groups = await getGroups(idUser);
+  if (idGroup) {
+    Bugfender.log("idUser:" + idUser);
+    let groups = await getGroups(idUser);
 
-        Bugfender.log('groups:' + groups.length);
+    Bugfender.log("groups:" + groups.length);
 
-        let group = groups.find(_ => _.Id.toString() === idGroup);
+    let group = groups.find((_) => _.Id.toString() === idGroup);
 
-        Bugfender.log('group:' + JSON.stringify(group));
+    Bugfender.log("group:" + JSON.stringify(group));
 
-        return checkGroupConsistency(group);
-    }
-    
-    return undefined;
-}
+    return checkGroupConsistency(group);
+  }
+
+  return undefined;
+};
 
 const getGroups = async (idUser: string) => {
-    const data = getLocalGroups() as IGroup[] || [];
+  const data = (getLocalGroups() as IGroup[]) || [];
 
-    Bugfender.log('local getGroups:' + data.length);
+  Bugfender.log("local getGroups:" + data.length);
 
-    let groups = data;
+  let groups = data;
 
-    if (data.length === 0)
-        groups = (await getUserFromAPI(idUser) as IUser).Groups;
+  if (data.length === 0)
+    groups = ((await getUserFromAPI(idUser)) as IUser).Groups;
 
-    return groups;
-}
+  return groups;
+};
 
 const setLocalGroup = (group: IGroup) => {
-    if (group != null && group.Name != undefined && group.Name != '') {
-        const groups = getLocalGroups().filter(_ => _.Id !== group.Id);
+  if (group != null && group.Name != undefined && group.Name != "") {
+    const groups = getLocalGroups().filter((_) => _.Id !== group.Id);
 
-        group.LastModified = new Date();
-        groups.push(group);
-
-        setLocalGroups(groups);
-
-        return true;
-    }
-}
-
-const setGroup = async (idUser: string, group: IGroup, doCloud: boolean = false) => {
-
-    if (!setLocalGroup(group))
-        return;
-
-    if (doCloud)
-    {
-        const groups = getLocalGroups().filter(_ => _.Id !== group.Id);
-        await mutationPutUser(new User(idUser, groups));
-    }
-        
-}
-
-const setSync = async (idUser: string) => {
-    let groupsFromCoud = (await getUserFromAPI(idUser) as IUser).Groups;
-
-    let groupsLocal = getLocalGroups();
-
-    if (groupsFromCoud.length >= groupsLocal.length) {
-        groupsFromCoud.forEach(groupCloud => {
-            groupCloud.LastModified = groupCloud.LastModified===undefined?new Date():groupCloud.LastModified;
-
-            const _local = groupsLocal.find(_ => _.Id === groupCloud.Id);
-
-            if (_local === undefined)
-                groupsLocal.push(groupCloud);
-            else
-                if (_local.LastModified < groupCloud.LastModified)
-                    {
-                        groupsLocal = groupsLocal.filter(_=>_.Id!==groupCloud.Id);
-                        groupsLocal.push(groupCloud);
-                    }
-        });
-    }
-    else
-        groupsLocal.forEach(groupLocal => {
-            const _cloud = groupsFromCoud.find(_ => _.Id === groupLocal.Id);
-
-            if (_cloud !== undefined && _cloud.LastModified < groupLocal.LastModified)
-            {
-                groupsLocal = groupsLocal.filter(_=>_.Id!==groupLocal.Id);
-                groupsLocal.push(_cloud);
-            }
-        });
-
-    setLocalGroups(groupsLocal);
-}
-
-const setGroups = async (idUser: string) => {
-    const groups = getLocalGroups();
-
-    await mutationPutUser(new User(idUser, groups));
-}
-
-const deleteGroup = async (idUser: string, group: IGroup, doCloud: boolean = false) => {
-    const groups = getLocalGroups().filter(_ => _.Id !== group.Id);
+    group.LastModified = new Date();
+    groups.push(group);
 
     setLocalGroups(groups);
 
-    if (doCloud)
-        await mutationPutUser(new User(idUser, groups));
-}
+    return true;
+  }
+};
 
-const historify = async (idUser:string, group: IGroup) => {
-    const _HistoryGroup = getLocalGroups().find(_ => _.Id === Group.HISTORY_ID) || Group.NewGroupHistory();
-    const wordsLearned = group.Words.filter(_=>_.IsKnowed && _.Cycles === 0);
+const setGroup = async (
+  idUser: string,
+  group: IGroup,
+  doCloud: boolean = false
+) => {
+  if (!setLocalGroup(group)) return;
 
-    group.Words = group.Words.filter(_=>!(_.IsKnowed && _.Cycles === 0));
+  if (doCloud) {
+    const groups = getLocalGroups().filter((_) => _.Id !== group.Id);
+    await mutationPutUser(new User(idUser, groups));
+  }
+};
 
-    setGroup(idUser, group);
+const setSync = async (idUser: string) => {
+  let groupsFromCoud = ((await getUserFromAPI(idUser)) as IUser).Groups;
 
-    _HistoryGroup.Words.push(... wordsLearned);
+  let groupsLocal = getLocalGroups();
 
-    setGroup(idUser, _HistoryGroup);
+  if (groupsFromCoud.length >= groupsLocal.length) {
+    groupsFromCoud.forEach((groupCloud) => {
+      groupCloud.LastModified =
+        groupCloud.LastModified === undefined
+          ? new Date()
+          : groupCloud.LastModified;
 
-}
-const setUser = (user:IUserInfo) => {
-    localStorage.setItem('__user', JSON.stringify(user));
-}
+      const _local = groupsLocal.find((_) => _.Id === groupCloud.Id);
+
+      if (_local === undefined) groupsLocal.push(groupCloud);
+      else if (_local.LastModified < groupCloud.LastModified) {
+        groupsLocal = groupsLocal.filter((_) => _.Id !== groupCloud.Id);
+        groupsLocal.push(groupCloud);
+      }
+    });
+  } else
+    groupsLocal.forEach((groupLocal) => {
+      const _cloud = groupsFromCoud.find((_) => _.Id === groupLocal.Id);
+
+      if (
+        _cloud !== undefined &&
+        _cloud.LastModified < groupLocal.LastModified
+      ) {
+        groupsLocal = groupsLocal.filter((_) => _.Id !== groupLocal.Id);
+        groupsLocal.push(_cloud);
+      }
+    });
+
+  setLocalGroups(groupsLocal);
+};
+
+const mutationGroups = async (idUser: string) => {
+  const groups = getLocalGroups();
+
+  await mutationPutUser(new User(idUser, groups));
+};
+
+const deleteGroup = async (
+  idUser: string,
+  group: IGroup,
+  doCloud: boolean = false
+) => {
+  const groups = getLocalGroups().filter((_) => _.Id !== group.Id);
+
+  setLocalGroups(groups);
+
+  if (doCloud) await mutationPutUser(new User(idUser, groups));
+};
+
+const historify = async (idUser: string, group: IGroup) => {
+  const _HistoryGroup =
+    getLocalGroups().find((_) => _.Id === Group.HISTORY_ID) ||
+    Group.NewGroupHistory();
+  const wordsLearned = group.Words.filter((_) => _.IsKnowed && _.Cycles === 0);
+
+  group.Words = group.Words.filter((_) => !(_.IsKnowed && _.Cycles === 0));
+
+  setGroup(idUser, group);
+
+  _HistoryGroup.Words.push(...wordsLearned);
+
+  setGroup(idUser, _HistoryGroup);
+};
+const setUser = (user: IUserInfo) => {
+  localStorage.setItem("__user", JSON.stringify(user));
+};
 
 const getUser = () => {
-    return JSON.parse(localStorage.getItem('__user') || JSON.stringify(globalUserDefault)) as unknown as IUserInfo;
-}
+  return JSON.parse(
+    localStorage.getItem("__user") || JSON.stringify(globalUserDefault)
+  ) as unknown as IUserInfo;
+};
 
 const cleanLocalGroups = () => {
-    setLocalGroups([]);
-}
+  setLocalGroups([]);
+};
 
 const getLocalGroups = (): IGroup[] => {
+  let data = localStorage.getItem("groups") as string;
 
-    let data = localStorage.getItem('groups') as string;
+  const groups = data ? JSON.parse(data) : undefined;
 
-    const groups = (data)?JSON.parse(data):undefined;
-
-    return groups;
-}
+  return groups;
+};
 
 const setLocalGroups = (groups: IGroup[]) => {
-
-    localStorage.setItem('groups', JSON.stringify(groups));
-
-}   
+  localStorage.setItem("groups", JSON.stringify(groups));
+};
 
 export const Adapter = {
-    getGroup
-    , getGroups
-    , setGroup
-    , deleteGroup
-    , setGroups
-    , setSync
-    , historify
-    , setUser
-    , getUser
-    , cleanLocalGroups
-    , setLocalGroup
-    , getLocalGroups
-    , setLocalGroups
-}
+  getGroup,
+  getGroups,
+  setGroup,
+  deleteGroup,
+  mutationGroups: mutationGroups,
+  setSync,
+  historify,
+  setUser,
+  getUser,
+  cleanLocalGroups,
+  setLocalGroup,
+  getLocalGroups,
+  setLocalGroups,
+};
--- src/pages/groups/GroupList.tsx
+++ src/pages/groups/GroupList.tsx
@@ -1,264 +1,308 @@
-import { Avatar, Box, Button, Divider, Grid, IconButton, List, ListItem, ListItemAvatar, ListItemText, TextField, Typography } from '@mui/material'
-import React, { FC, useContext } from 'react';
-import { useEffect, useState } from 'react';
+import {
+  Avatar,
+  Box,
+  Button,
+  Divider,
+  Grid,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { FC, useContext } from "react";
+import { useEffect, useState } from "react";
 
-import { setLocalGroups } from '../../locals/group.local';
+import { setLocalGroups } from "../../locals/group.local";
 
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
-import CasesOutlinedIcon from '@mui/icons-material/CasesOutlined';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import CasesOutlinedIcon from "@mui/icons-material/CasesOutlined";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 
-import { Link, useNavigate, useParams } from 'react-router-dom';
-import { IGroup, IGroupProps } from '../../interfaces/IGroup';
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { IGroup, IGroupProps } from "../../interfaces/IGroup";
 // import { UserContext } from "../../context/context.create";
-import { Adapter } from '../../locals/adapter';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import BackupIcon from '@mui/icons-material/Backup';
-import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
-import { makeStyles } from '@material-ui/styles';
-import Header from '../../components/Header';
-import DeleteButton from '../../elements/DeleteButton/Index';
-import ConfirmationDialog from '../../elements/Dialogs/ConfirmationDialog';
-import { MessageDialog } from '../../elements/Dialogs/MessageDialog';
-import { UserContext } from '../../context/context.user';
-import { filterWordByWord } from '../../util/util';
-import { Bugfender } from '@bugfender/sdk';
+import { Adapter } from "../../locals/adapter";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import BackupIcon from "@mui/icons-material/Backup";
+import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
+import { makeStyles } from "@material-ui/styles";
+import Header from "../../components/Header";
+import DeleteButton from "../../elements/DeleteButton/Index";
+import ConfirmationDialog from "../../elements/Dialogs/ConfirmationDialog";
+import { MessageDialog } from "../../elements/Dialogs/MessageDialog";
+import { UserContext } from "../../context/context.user";
+import { filterWordByWord } from "../../util/util";
+import { Bugfender } from "@bugfender/sdk";
 
 const useStyles = makeStyles({
-    button: {
-        margin: '8px 8px 8px 0'
-        , textAlign: 'right'
-    },
+  button: {
+    margin: "8px 8px 8px 0",
+    textAlign: "right",
+  },
 
-    list: {
-        maxWidth: 'none'
-    }
+  list: {
+    maxWidth: "none",
+  },
 });
 
-const ItemGroup: FC<IGroupProps> = ({ item, filter, deleteGroup }: IGroupProps): JSX.Element => {
+const ItemGroup: FC<IGroupProps> = ({
+  item,
+  filter,
+  deleteGroup,
+}: IGroupProps): JSX.Element => {
+  const { userInfo, updateValue } = useContext(UserContext);
+  const navigate = useNavigate();
+  const handlePlayClick = (id: string) => {
+    userInfo.PlayingGroup = id;
 
-    const { userInfo, updateValue } = useContext(UserContext);
-    const navigate = useNavigate();
-    const handlePlayClick = (id: string) => {
+    updateValue(userInfo);
 
-        userInfo.PlayingGroup = id;
+    navigate(`/play`);
+  };
 
-        updateValue(userInfo);
+  function handleButtonDelete(item: IGroup): void {
+    Adapter.deleteGroup(userInfo.UserId, item);
+    deleteGroup(item);
+  }
 
-        navigate(`/play`);
-    }
+  const handleSaveButtonEdit = (item: IGroup): void => {
+    navigate(`/group/${item.Id.toString()}/${filter}`);
+  };
 
-    function handleButtonDelete(item: IGroup): void {
-
-        Adapter.deleteGroup(userInfo.UserId, item);
-        deleteGroup(item);
-
-    }
-
-    const handleSaveButtonEdit = (item: IGroup): void => {
-        navigate(`/group/${item.Id.toString()}/${filter}`)
-    }
-
-    return (
-        <ListItem alignItems="flex-start">
-            <ListItemAvatar style={{alignSelf: 'center'}} id='listAvatar'>
-                {/* <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" /> */}
-                <CasesOutlinedIcon></CasesOutlinedIcon>
-            </ListItemAvatar>
-            <ListItemText
-                primary={item.Name}
-                secondary={
-                    <React.Fragment>
-                        <Typography
-                            sx={{ display: 'inline' }}
-                            component="span"
-                            variant="body2"
-                            color="text.primary"
-                        />
-                        <span style={{display:'flex'}}>
-                            <div>
-                                {item.Words.length} total |
-                            </div>
-                            <div>
-                                &nbsp;{`${item.Words.filter(_=>_.IsKnowed).length}`} learned |
-                            </div>
-                            <div>
-                                &nbsp;{`${item.Words.filter(_=>_.Cycles).length}`} Cycle
-                            </div>
-                        </span>
-                        <span style={{ display: 'flex', alignItems: 'right' }}>
-
-                            <DeleteButton handleDeleteItem={handleButtonDelete} item={item}></DeleteButton>
-                            <IconButton onClick={() => handleSaveButtonEdit(item)}>
-                                <EditIcon />
-                            </IconButton>
-                        </span>
-                    </React.Fragment>
-                }
+  return (
+    <ListItem alignItems="flex-start">
+      <ListItemAvatar style={{ alignSelf: "center" }} id="listAvatar">
+        {/* <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" /> */}
+        <CasesOutlinedIcon></CasesOutlinedIcon>
+      </ListItemAvatar>
+      <ListItemText
+        primary={item.Name}
+        secondary={
+          <React.Fragment>
+            <Typography
+              sx={{ display: "inline" }}
+              component="span"
+              variant="body2"
+              color="text.primary"
             />
-            {userInfo.PlayingGroup !== item.Id && <div style={{ alignSelf: 'center' }}>
-                <Button variant="outlined"
-                    onClick={() => handlePlayClick(item.Id)}
-                    startIcon={<PlayArrowIcon />}>
-                    Play
-                </Button>
-            </div>}
-        </ListItem>
-    )
+            <span style={{ display: "flex" }}>
+              <div>{item.Words.length} total |</div>
+              <div>
+                &nbsp;{`${item.Words.filter((_) => _.IsKnowed).length}`} learned
+                |
+              </div>
+              <div>
+                &nbsp;{`${item.Words.filter((_) => _.Cycles).length}`} Cycle
+              </div>
+            </span>
+            <span style={{ display: "flex", alignItems: "right" }}>
+              <DeleteButton
+                handleDeleteItem={handleButtonDelete}
+                item={item}
+              ></DeleteButton>
+              <IconButton onClick={() => handleSaveButtonEdit(item)}>
+                <EditIcon />
+              </IconButton>
+            </span>
+          </React.Fragment>
+        }
+      />
+      {userInfo.PlayingGroup !== item.Id && (
+        <div style={{ alignSelf: "center" }}>
+          <Button
+            variant="outlined"
+            onClick={() => handlePlayClick(item.Id)}
+            startIcon={<PlayArrowIcon />}
+          >
+            Play
+          </Button>
+        </div>
+      )}
+    </ListItem>
+  );
+};
+function GroupListComponent(
+  groupList: any[],
+  filter: string,
+  deleteGroup: (item: IGroup) => void
+) {
+  return (
+    <Box style={{ height: "calc(100vh - 260px)", overflow: "auto" }}>
+      <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+        {groupList.map((item, i) => {
+          return (
+            <React.Fragment key={i}>
+              <ItemGroup
+                item={item}
+                deleteGroup={deleteGroup}
+                filter={filter}
+              ></ItemGroup>
+              <Divider variant="inset" component="li" />
+            </React.Fragment>
+          );
+        })}
+      </List>
+    </Box>
+  );
 }
-function GroupListComponent(groupList: any[], filter: string, deleteGroup: (item: IGroup) => void) {
-
-    return (
-        <Box style={{ height: 'calc(100vh - 260px)', overflow: 'auto' }}>
-            <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
-                {
-                    groupList.map((item, i) => {
-                        return (
-                            <React.Fragment key={i}>
-                                <ItemGroup item={item} deleteGroup={deleteGroup} filter={filter}></ItemGroup><Divider variant="inset" component="li" />
-                            </React.Fragment>
-                        )
-                    })
-                }
-            </List>
-        </Box>
-    );
-}
-
 
 export const GroupList = () => {
-    const classes = useStyles();
-    const { userInfo, updateValue } = useContext(UserContext);
-    const navigate = useNavigate();
-    const [groups, setGroups] = useState<IGroup[]>([]);
-    const [dataGroups, setDataGroups] = useState<IGroup[]>([]);
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [isActiveMessageSaveData, setIsActiveMessageSaveData] = useState<boolean>(false);
-    const [isSyncSuccessful, setIsSyncSuccessful] = useState<boolean>(false);
-    const [messageSuccessful, setMessageSuccessful] = useState<string>('');
-    const [filter, setFilter] = useState('');
+  const classes = useStyles();
+  const { userInfo, updateValue } = useContext(UserContext);
+  const navigate = useNavigate();
+  const [groups, setGroups] = useState<IGroup[]>([]);
+  const [dataGroups, setDataGroups] = useState<IGroup[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isActiveMessageSaveData, setIsActiveMessageSaveData] =
+    useState<boolean>(false);
+  const [isSyncSuccessful, setIsSyncSuccessful] = useState<boolean>(false);
+  const [messageSuccessful, setMessageSuccessful] = useState<string>("");
+  const [filter, setFilter] = useState("");
 
-    let { word } = useParams();
+  let { word } = useParams();
 
-    const getData = async () => {
-        setIsLoading(true);
+  const getData = async () => {
+    let _groups = (await Adapter.getGroups(userInfo.UserId)) as IGroup[];
 
-        let _groups = await Adapter.getGroups(userInfo.UserId) as IGroup[];
+    setLocalGroups(_groups);
+    setDataGroups(_groups);
 
-        setLocalGroups(_groups);
-        setDataGroups(_groups);
+    setGroups(_groups);
 
-        setGroups(_groups);
-    };
+    setIsLoading(true);
+  };
 
-    useEffect(() => {
-        getData();
-        setFilter(word || '');
+  useEffect(() => {
+    getData();
+    setFilter(word || "");
+  }, []);
 
-    }, []);
+  useEffect(() => {
+    if (groups) setIsLoading(false);
+  }, [groups]);
 
-    useEffect(() => {
-        if (groups)
-            setIsLoading(false);
-    }, [groups]);
+  useEffect(() => {
+    Bugfender.log("Pass1");
+    if (filter !== undefined) {
+      Bugfender.log("Pass2");
+      Bugfender.log("dataGroups:" + dataGroups.length);
+      let _groups = dataGroups.filter((_group) => {
+        Bugfender.log("Pass3");
+        Bugfender.log(JSON.stringify(_group));
+        let _filter = _group.Words.filter(
+          (word) =>
+            filterWordByWord(word.Name, filter) ||
+            filterWordByWord(word.Value, filter)
+        );
 
-    useEffect(() => {
-        Bugfender.log('Pass1');
-        if (filter !== undefined) {
-            Bugfender.log('Pass2');
-            Bugfender.log('dataGroups:' + dataGroups.length);
-            let _groups = dataGroups.filter(_group => {
-                Bugfender.log('Pass3');
-                Bugfender.log(JSON.stringify(_group));
-                let _filter = _group.Words.filter(word => filterWordByWord(word.Name, filter) || filterWordByWord(word.Value, filter));
-
-                return _filter.length > 0 || _group.Name.toLocaleLowerCase().indexOf(filter.toLocaleLowerCase())>=0;
-            });
-            setGroups(_groups);
-        }
-    }, [filter])
-
-
-    function handleAddClick(): void {
-        navigate('/group');
+        return (
+          _filter.length > 0 ||
+          _group.Name.toLocaleLowerCase().indexOf(filter.toLocaleLowerCase()) >=
+            0
+        );
+      });
+      setGroups(_groups);
     }
+  }, [filter, dataGroups]);
 
-    const deleteGroup = (item: IGroup) => {
-        setGroups((prev) => {
-            return [...prev.filter((_) => _.Id !== item.Id)];
-        })
-    }
+  function handleAddClick(): void {
+    navigate("/group");
+  }
 
-    function handleSaveAction(): void {
-        Adapter.setGroups(userInfo.UserId);
-        // Adapter.setDrive(userInfo);
-        setIsActiveMessageSaveData(false);
-        setIsSyncSuccessful(true);
-        setMessageSuccessful('Upload process was complete succesfull.');
-    }
+  const deleteGroup = (item: IGroup) => {
+    setGroups((prev) => {
+      return [...prev.filter((_) => _.Id !== item.Id)];
+    });
+  };
 
-    const handleSync = async () => {
-        await Adapter.setSync(userInfo.UserId);
+  function handleSaveAction(): void {
+    Adapter.mutationGroups(userInfo.UserId);
+    // Adapter.setDrive(userInfo);
+    setIsActiveMessageSaveData(false);
+    setIsSyncSuccessful(true);
+    setMessageSuccessful("Upload process was complete succesfull.");
+  }
 
-        let _groups = await Adapter.getGroups(userInfo.UserId) as IGroup[];
+  const handleSync = async () => {
+    await Adapter.setSync(userInfo.UserId);
 
-        setGroups(_groups);
+    let _groups = (await Adapter.getGroups(userInfo.UserId)) as IGroup[];
 
-        setIsSyncSuccessful(true);
-        setMessageSuccessful('Sync process was complete succesfull.');
-    }
+    setGroups(_groups);
 
-    return (<div>
+    setIsSyncSuccessful(true);
+    setMessageSuccessful("Sync process was complete succesfull.");
+  };
 
-        <Header title="My Collections" />
+  return (
+    <div>
+      <Header title="My Collections" />
 
-        <MessageDialog
-            open={isSyncSuccessful}
-            // title="Sync successful!"
-            message={messageSuccessful}
-            onClose={() => setIsSyncSuccessful(false)}
-        />
+      <MessageDialog
+        open={isSyncSuccessful}
+        // title="Sync successful!"
+        message={messageSuccessful}
+        onClose={() => setIsSyncSuccessful(false)}
+      />
 
-        <ConfirmationDialog message="Are you sure you want to save your data in the cloud?" onConfirm={handleSaveAction} open={isActiveMessageSaveData} onClose={() => setIsActiveMessageSaveData(false)} />
+      <ConfirmationDialog
+        message="Are you sure you want to save your data in the cloud?"
+        onConfirm={handleSaveAction}
+        open={isActiveMessageSaveData}
+        onClose={() => setIsActiveMessageSaveData(false)}
+      />
 
-        <Grid container alignItems="center" justifyContent="space-between">
-            <Grid item xs={9} sm={9} >
-                <TextField id="standard-basic"
-                    label="Filter" variant="standard"
-                    style={{ width: '100%' }}
-                    onChange={(e) => setFilter(e.target.value)}
-                    value={filter} />
-            </Grid>
-            <Grid container item xs={3} sm={3}>
-                <Grid item xs={6} sm={6}>
-                    {userInfo.IsInLogin && <IconButton onClick={() => handleSync()}>
-                        <CloudDownloadIcon />
-                    </IconButton>}
-                </Grid>
-                <Grid item>
-                    {userInfo.IsInLogin && <IconButton onClick={() => setIsActiveMessageSaveData(true)}>
-                        <BackupIcon />
-                    </IconButton>}
-                </Grid>
-            </Grid>
-
+      <Grid container alignItems="center" justifyContent="space-between">
+        <Grid item xs={9} sm={9}>
+          <TextField
+            id="standard-basic"
+            label="Filter"
+            variant="standard"
+            style={{ width: "100%" }}
+            onChange={(e) => setFilter(e.target.value)}
+            value={filter}
+          />
         </Grid>
-        <div>
-            {isLoading ? (
-                'Loading groups...'
-            ) : (
-                <div>
-                    {GroupListComponent(groups, filter, deleteGroup)}
-                </div>
+        <Grid container item xs={3} sm={3}>
+          <Grid item xs={6} sm={6}>
+            {userInfo.IsInLogin && (
+              <IconButton onClick={() => handleSync()}>
+                <CloudDownloadIcon />
+              </IconButton>
             )}
+          </Grid>
+          <Grid item>
+            {userInfo.IsInLogin && (
+              <IconButton onClick={() => setIsActiveMessageSaveData(true)}>
+                <BackupIcon />
+              </IconButton>
+            )}
+          </Grid>
+        </Grid>
+      </Grid>
+      <div>
+        {isLoading ? (
+          "Loading groups..."
+        ) : (
+          <div>{groups && GroupListComponent(groups, filter, deleteGroup)}</div>
+        )}
+      </div>
+      <div>
+        <div className={classes.button}>
+          <IconButton
+            aria-label="add"
+            size="large"
+            color="success"
+            onClick={handleAddClick}
+          >
+            <AddCircleIcon fontSize="inherit" sx={{ fontSize: 40 }} />
+          </IconButton>
         </div>
-        <div>
-            <div className={classes.button}>
-                <IconButton aria-label="add" size="large" color="success" onClick={handleAddClick}>
-                    <AddCircleIcon fontSize="inherit" sx={{ fontSize: 40 }} />
-                </IconButton>
-            </div>
-        </div>
-    </div>)
-}
+      </div>
+    </div>
+  );
+};
