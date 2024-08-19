5ed4ae726783d130d7c959f1be26a8e8458ef556
Rodrigo Jara <rodrigo.jara@raisin.com>
2024-08-20 01:20:45 +0200

    adding secrets

--- src/constants/drive.tsx
+++ src/constants/drive.tsx
@@ -1,4 +1,5 @@
 export const _DRIVE = {
     DRIVE_FILE_PREFIX: "glimmind_"
     , DRIVE_NAME_FOLDER: "glimmind.com"
+    , REACT_APP_GOOGLE_CLIENT_ID: "86552858818-uaavifev1toq70ckjp0ddol9lmk0v2oj.apps.googleusercontent.com"
 }
